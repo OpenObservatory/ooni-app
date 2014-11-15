8cfb93d4abb724f9c49071fba269bba3ac4dcd7b
Arturo Filastò <art@fuffa.org>
2014-11-15 01:00:05 +0100

    Add docker usage and improve infinite scrolling.

--- README.md
+++ README.md
@@ -19,3 +19,12 @@ grunt
 The dev server should now be listening on port 3000.
 
 Have fun!
+
+# Running in docker
+
+You can run this inside of docker by running from the home of this repo:
+
+```
+docker build -t ooni/mean .
+docker run -d --name="ooni-mean-1" -p 3000:3000 ooni/mean
+```
--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'ooni-mean';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'infinite-scroll'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
