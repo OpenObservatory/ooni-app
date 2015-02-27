a21e5aaeaabb4afd0f1dbd1412426c4aa59eac84
Arturo Filastò <art@fuffa.org>
2015-02-27 16:18:15 +0100

    Move docker script into docker folder
    Create docker file to rebuild app

---
+++ docker/ooni-app-builder/rebuild.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+npm install
+bower install --config.interactive=false --allow-root
---
+++ docker/ooni-app-builder/start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run -d -v /root/ooni-app/public/:/home/mean/public ooni-app-rebuilder
