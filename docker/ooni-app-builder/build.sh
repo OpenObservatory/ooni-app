5122a9bc73974c57167898ab1126634e7eb42233
Arturo Filastò <art@fuffa.org>
2015-02-27 16:40:01 +0100

    Implement workaround problem with docker: https://github.com/docker/docker/issues/2745

--- docker/ooni-app-builder/build.sh
+++ docker/ooni-app-builder/build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+cd ../../
+cp docker/ooni-app-builder/Dockerfile .
 docker build -t ooni-app-rebuilder .
+rm Dockerfile
--- docker/ooni-app/build.sh
+++ docker/ooni-app/build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+cd ../../
+cp docker/ooni-app/Dockerfile .
 docker build -t ooni-app .
+rm Dockerfile
