f49af87687ad184c20cd61e57204c275e20b301e
Arturo Filastò <art@fuffa.org>
2015-02-27 16:19:21 +0100

    Share public and app folder

--- docker/ooni-app/start.sh
+++ docker/ooni-app/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -p 104.193.9.122:3000:3000 --link mongodb:db ooni-app
+docker run -d -p 104.193.9.122:3000:3000 -v /root/ooni-app/public/:/home/mean/public -v /root/ooni-app/app/:/home/mean/app --link mongodb:db ooni-app
